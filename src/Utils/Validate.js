75e5470712ea6ce7c8fb1ce0181836cf3e1cdae2
Rahul C <98203345+Rahul172001@users.noreply.github.com>
2026-09-24 20:15:21 +0530

    Add files via upload

---
+++ src/Utils/Validate.js
@@ -0,0 +1,6 @@
+export const checkValidate = (email,password)=>{
+    const validateEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)
+    const validatePassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password)
+    if(!validateEmail || !validatePassword) return "Invalid username or password"
+    return null
+}
